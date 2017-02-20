1cf703bb858823b04338fe1ce97e32d5793f638f
Rashed <rashedcs@users.noreply.github.com>
2017-02-21 01:31:42 +0600

    Update Install Cisco Pocket Tracer.cpp

--- Install Cisco Pocket Tracer.cpp
+++ Install Cisco Pocket Tracer.cpp
@@ -2,3 +2,4 @@ link :
 https://www.packet6.com/how-to-install-packet-tracer-on-ubuntu-linux/
 https://www.youtube.com/watch?v=wBdOxBlMqSI
 
+http://howto.blogz.tech/493565394b5958664a666b/install-and-uninstall-packettracer-in-ubuntu-1204.html

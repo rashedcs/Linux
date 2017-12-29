89977a0595d1b21266e2901f179da2f9f3496c02
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-12-30 00:13:36 +0600

    static routing

--- Networking/Assignments/01.sh
+++ Networking/Assignments/01.sh
@@ -1,66 +1,63 @@
 Problem Name : Implement static routing  
 
-
 Solution :
+    First Set up individual pc ip address Then follow the individual steps
 
-First Set up individual pc ip address Then follow the individual steps
+    Step 1:
+    #Router 0 interface command lines are given below :
 
+    Router>enable
+    Router#configure terminal
 
-Step 1:
-#Router 0 interface command lines are given below :
+    Router(config)#interface fastEthernet 0/0
+    Router(config-if)#ip address 192.168.12.1 255.255.255.0
+    Router(config-if)#no shutdown
 
-Router>enable
-Router#configure terminal
+    Router(config-if)#exit
 
-Router(config)#interface fastEthernet 0/0
-Router(config-if)#ip address 192.168.12.1 255.255.255.0
-Router(config-if)#no shutdown
-
-Router(config-if)#exit
-
-Router(config)#interface fastEthernet 0/1
-Router(config-if)#ip address 192.168.10.1 255.255.255.0
-Router(config-if)#no shutdown
-Router(config-if)#exit
-Router(config)#exit
-Router#
+    Router(config)#interface fastEthernet 0/1
+    Router(config-if)#ip address 192.168.10.1 255.255.255.0
+    Router(config-if)#no shutdown
+    Router(config-if)#exit
+    Router(config)#exit
+    Router#
 
 
 
-Step 2:
-#Router 1 interface command lines are given below :
+    Step 2:
+    #Router 1 interface command lines are given below :
 
-Router>enable
-Router#configure terminal
+    Router>enable
+    Router#configure terminal
 
-Router(config)#interface fastEthernet 0/0
-Router(config-if)#ip address 192.168.12.1 255.255.255.0
-Router(config-if)#no shutdown
+    Router(config)#interface fastEthernet 0/0
+    Router(config-if)#ip address 192.168.12.1 255.255.255.0
+    Router(config-if)#no shutdown
 
-Router(config-if)#exit
+    Router(config-if)#exit
 
-Router(config)#interface fastEthernet 0/1
-Router(config-if)#ip address 192.168.10.1 255.255.255.0
-Router(config-if)#no shutdown
-Router(config-if)#exit
-Router(config)#exit
-Router#
-
-
-
-Step 3:
-##Router 0 routing configuration  command lines are given below :
-
-Router#configure terminal
-Router(config)#ip route 192.168.11.0 255.255.255.0 192.168.12.2
-Router(config)#exit
+    Router(config)#interface fastEthernet 0/1
+    Router(config-if)#ip address 192.168.10.1 255.255.255.0
+    Router(config-if)#no shutdown
+    Router(config-if)#exit
+    Router(config)#exit
+    Router#
 
 
 
-Step 4:
-##Router 0 routing configuration  command lines are given below :
+    Step 3:
+    ##Router 0 routing configuration  command lines are given below :
 
-Router#configure terminal
-Router(config)#ip route 192.168.11.0 255.255.255.0 192.168.12.2
-Router(config)#exit
+    Router#configure terminal
+    Router(config)#ip route 192.168.11.0 255.255.255.0 192.168.12.2
+    Router(config)#exit
+
+
+
+    Step 4:
+    ##Router 0 routing configuration  command lines are given below :
+
+    Router#configure terminal
+    Router(config)#ip route 192.168.11.0 255.255.255.0 192.168.12.2
+    Router(config)#exit
 

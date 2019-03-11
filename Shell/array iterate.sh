99fa12be99f0b4cc2a9eecdde69650f3d4f9b74d
Rashedul Islam <rashedcs@users.noreply.github.com>
2019-03-11 17:13:16 +0600

    Update array iterate.sh

--- Shell/array iterate.sh
+++ Shell/array iterate.sh
@@ -10,3 +10,24 @@ function iterate
 
 array=(1 2 3)
 iterate ${array[@]}
+
+
+
+
+
+
+
+
+
+Another way :
+
+function iterate 
+{
+    for i in "${a[@]}"
+    do
+        echo $i  
+    done
+}
+
+a=(7 2 3 9)
+iterate ${a[@]}
